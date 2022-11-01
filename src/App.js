7d945934ed13fe51e48240643b76ce14b6c79d12
Eduards Birznieks <eduards.latvia@gmail.com>
2022-11-01 14:20:01 +0100

    New Styling
    Kind of nice, new h1 and changed logo not really inpresive

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
+import gear from './gear.svg';
 import './App.css';
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import CookieBot from 'react-cookiebot';
+
 const domainGroupId = 'd9eb7ea3-2545-41f3-8efb-7e9a03076eb1';
 
+
 function App() {
-  const [hasCookieBot, setHasCookieBot] = useState(undefined);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <CookieBot domainGroupId={domainGroupId} />
+        <img src={gear} className="App-logo" alt="gearSpinnig" />
+        <h1 className='bsmain bs' >
+          BIKE <spna className="bs2">SUPPORT</spna> 
+        </h1>
         <p>Bikes need support to</p>
-          <CookieBot domainGroupId={domainGroupId} />
       </header>
     </div>
   );
